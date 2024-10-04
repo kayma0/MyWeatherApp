81879351619007a385c50492acd00c8988d01b51
kayma0 <131287457+kayma0@users.noreply.github.com>
2024-10-04 13:39:41 +0100

    added date

--- index.js
+++ index.js
@@ -13,8 +13,31 @@ function updateWeatherinfo(response) {
 
   let cityWind = document.querySelector("#T_wind");
   cityWind.innerHTML = `${response.data.wind.speed}km/h`;
+
+  let cityTime = document.querySelector("#city_time");
+  let date = new Date(response.data.time * 1000);
+  cityTime.innerHTML = formatDate(date);
 }
 
+function formatDate(date) {
+  let day = date.getDay();
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day1 = days[date.getDay()];
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day1}, ${hours}:${minutes}`;
+}
 function searchCity(city) {
   let apiKey = "c69690dffef27at0b043of215d340482";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
